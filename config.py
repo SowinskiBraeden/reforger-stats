28b6ab5f59eb7ee2ce7a216abebcea57eef59189
SowinskiBraeden <sowinskibraeden@gmail.com>
2024-11-17 14:58:08 -0800

    tidy config

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
   TOKEN:         str = os.getenv("DISCORD_TOKEN")
   GUILD:         str = os.getenv("DISCORD_GUILD")
   ADMIN:         str = int(os.getenv("ADMIN_ROLE"))
-  WEBHOOK:        str = os.getenv("WEBHOOK")
+  WEBHOOK:       str = os.getenv("WEBHOOK")
 
   SFTP_HOST:     str = os.getenv("SFTP_HOST")
   SFTP_PORT:     int = int(os.getenv("SFTP_PORT"))
